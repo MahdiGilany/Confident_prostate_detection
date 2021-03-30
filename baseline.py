6e9f964e6810ed26a9b50cde1d62f677fe7b8189
minh <minhto2802@gmail.com>
2021-03-30 12:43:48 -0700

    Except from transformer.py, other networks taken from resnet1d repo were not tested.

--- baseline.py
+++ baseline.py
@@ -4,59 +4,13 @@ from collections import namedtuple
 
 import torch
 import torch.nn as nn
-from torch import Tensor
 import torch.nn.functional as F
+
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 from utils.misc import load_pkl
 from utils.dataset import RandomSelect, ToTensor, RFDataset
-
-
-class TransformerModel(nn.Module):
-    def __init__(self, n_class, n_inp, n_head, n_hid, n_layers, dropout):
-        super(TransformerModel, self).__init__()
-        self.model_type = 'Transformer'
-        self.pos_encoder = PositionalEncoding(n_inp, dropout)
-        encoder_layers = TransformerEncoderLayer(n_inp, n_head, n_hid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, n_layers)
-        self.decoder = nn.Linear(n_inp, n_class)
-
-        self.init_weights()
-
-    def generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
-
-    def init_weights(self):
-        init_range = .1
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-init_range, init_range)
-
-    def forward(self, src):
-        src = self.pos_encoder(src)  # Positional embedding
-        output = self.transformer_encoder(src)
-        output = self.decoder(output)
-        return output
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
 
 
 def get_device(device_str=None):
@@ -85,24 +39,26 @@ def data_preparation():
     return data_train, data_val, data_test
 
 
-def get_model():
+def get_model(model_name):
     """
 
     :return:
     """
-    params = namedtuple('HyperParameters', [])
-    params.n_class = 2
-    params.em_size = 100  # embedding dimension
-    params.n_hid = 100  # the dimension of the feedforward network model in nn.TransformerEncoder
-    params.n_layers = 2  # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
-    params.n_head = 2  # the number of heads in the multi_head_attention models
-    params.dropout = .0  # the dropout value
-    model = TransformerModel(params.n_class, params.em_size, params.n_hid, params.n_head,
-                             params.n_layers, params.dropout).to(get_device())
-    return model, params
+    if model_name == 'transformer':
+        from models.transformer import TransformerModel
+        params = namedtuple('HyperParameters', [])
+        params.n_class = 2
+        params.em_size = 100  # embedding dimension
+        params.n_hid = 100  # the dimension of the feedforward network model in nn.TransformerEncoder
+        params.n_layers = 2  # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+        params.n_head = 2  # the number of heads in the multi_head_attention models
+        params.dropout = .0  # the dropout value
+        model = TransformerModel(params.n_class, params.em_size, params.n_hid, params.n_head,
+                                 params.n_layers, params.dropout).to(get_device())
+        return model, params
 
 
-def get_trainer(model: TransformerModel):
+def get_trainer(model: nn.Module):
     trainer = namedtuple('Trainer', ['criterion', 'optimizer', 'scheduler'])
     trainer.criterion = nn.CrossEntropyLoss()
     trainer.lr = 5.  # learning rate
@@ -111,7 +67,7 @@ def get_trainer(model: TransformerModel):
     return trainer
 
 
-def train(model: TransformerModel, n_class: int, data_train, epoch: int, trainer: namedtuple, device: torch.device):
+def train(model: nn.Module, n_class: int, data_train, epoch: int, trainer: namedtuple, device: torch.device):
     model.train()  # Turn on the train mode
     total_loss = 0.
     start_time = time.time()
@@ -141,7 +97,7 @@ def train(model: TransformerModel, n_class: int, data_train, epoch: int, trainer
     return model
 
 
-def evaluate(eval_model: TransformerModel, n_class: int, data_source, trainer: namedtuple, device: torch.device):
+def evaluate(eval_model: nn.Module, n_class: int, data_source, trainer: namedtuple, device: torch.device):
     eval_model.eval()
     total_loss = 0.
     acc = 0.
@@ -164,7 +120,7 @@ def main():
 
     train_data, val_data, test_data = data_preparation()
 
-    model, params = get_model()
+    model, params = get_model('transformer')
 
     trainer = get_trainer(model)
     best_val_loss = float('inf')
